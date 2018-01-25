123e723ec46511f42106e9fccd7f6632ef6b8773
Daniel No <mr.danielno@gmail.com>
2018-01-25 15:08:00 -0500

    2.2

--- ctci_chapter_2.playground/Contents.swift
+++ ctci_chapter_2.playground/Contents.swift
@@ -25,6 +25,7 @@ func removeDuplicates(linkedList : inout SinglyLinkedList) -> SinglyLinkedList{
 }
 /*
 2.1 Follow up : Remove duplicates no extra space/buffer
+ Approach : Iterate through array in one pass,track a node to check for duplicates, track prev and curr to allow deletion
  Time : O(n^2) - quadratic time
  Space : O(1) - constant time
  */
@@ -48,7 +49,38 @@ func removeDupesNoBuffer(linkedList : inout SinglyLinkedList) -> SinglyLinkedLis
     return linkedList
 }
 
-var linkedList = SinglyLinkedList(nums: [5,5,5,5,5,5,5,4,4,5])
-removeDupesNoBuffer(linkedList: &linkedList)
+/* 2.2 Return kth to last element in singly linked list
+ Approach : Iterate through linked list to find the size. Define a target value to hit on the second iteration of the linked list : LinkedList_Size - kth_to_last_element = Target_Node
+    Return the node if the target is reached, else return nil if the kth_to_last_element > size or if nothing is found
+ */
+
+func kthToLastElementBrute(head : Node?, k : Int) -> Node?{
+    var curr = head
+    var size = 0
+    var count = 0
+    while curr != nil {
+        size+=1
+        curr = curr?.next
+    }
+    if k > size {
+        return nil
+    }
+    let target = size - k
+    print("target \(target)")
+    curr = head
+    while curr != nil {
+        if target == count {
+            return curr
+        }else{
+            count+=1
+        }
+        curr = curr?.next
+    }
+    return nil
+}
+
+var linkedList = SinglyLinkedList(nums: [1,2,3,4,5,6])
+let n = kthToLastElementBrute(head: linkedList.head, k: 1)
+//removeDupesNoBuffer(linkedList: &linkedList)
 //removeDuplicates(linkedList: &linkedList)
 
