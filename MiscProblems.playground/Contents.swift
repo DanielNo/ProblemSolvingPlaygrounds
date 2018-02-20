4f0ba62c3d3cec8f22041353f95cb7ab61794a8f
Daniel No <mr.danielno@gmail.com>
2018-02-20 13:10:22 -0500

    max howell invert binary tree

--- MiscProblems.playground/Contents.swift
+++ MiscProblems.playground/Contents.swift
@@ -35,3 +35,45 @@ func swiftyFib(num : Int) -> Int{
 
 //fibonacci(num: 90)
 swiftyFib(num: 90)
+
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    public init(_ val: Int, left : Int, right : Int) {
+        self.val = val
+        self.left = TreeNode(left)
+        self.right = TreeNode(right)
+    }
+    
+    // Leetcode : Max Howell Invert binary tree : https://leetcode.com/problems/invert-binary-tree/description/
+    // Approach : Recursively swap left and right nodes of the tree
+    public func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root?.left == nil && root?.right == nil {
+            return root
+        }
+        var tree = root
+        let left = root?.left
+        let right = root?.right
+        tree?.left = right
+        tree?.right = left
+        invertTree(tree?.left)
+        invertTree(tree?.right)
+        return tree
+    }
+    
+}
+var tree = TreeNode(1, left: 2, right: 3)
+var left = tree.left?.val
+var right = tree.right?.val
+tree.invertTree(tree)
+var left2 = tree.left?.val
+var right2 = tree.right?.val
+
