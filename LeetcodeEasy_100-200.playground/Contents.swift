e9198386e5c08c2888127a872f33d8f0b3e487ce
Daniel No <mr.danielno@gmail.com>
2018-04-16 19:09:54 -0400

    136 single num

--- LeetcodeEasy_100-200.playground/Contents.swift
+++ LeetcodeEasy_100-200.playground/Contents.swift
@@ -43,5 +43,29 @@ func generate(numRows : Int) -> [[Int]]{
     return triangle
     
 }
-let x = generate(numRows: 4)
-print(x)
+let rows = generate(numRows: 1)
+
+/*
+ 136. Single Number
+ Approach : Looking for a single number in an array with all others being duplicates.
+ Using XOR num xor 0 will return num.
+ Using XOR on a different number will add the result.
+ Using XOR on a duplicate number will subtract the result.
+ Basically using xor on this number set will decide to either add or subtract the given number and will return the single occurence number.
+ 
+ Time Complexity : O(n)
+ Space Complexity : O(1)
+ 
+ */
+func singleNumber(_ nums: [Int]) -> Int {
+    var res = 0
+    for num in nums{
+        res = res ^ num
+        print(res)
+    }
+    return res
+}
+let nums = [1,5,6,5,6,3,1]
+let singleNum = singleNumber(nums)
+
+
