1bd5133aff8b169c472e2da418215435c95e07e8
Daniel No <mr.danielno@gmail.com>
2019-06-02 14:19:59 -0400

    optimize a bit

--- Distribution.playground/Contents.swift
+++ Distribution.playground/Contents.swift
@@ -4,24 +4,27 @@ var arr1 = ["A","D","G","I"]
 var arr2 = ["B","E"]
 var arr3 = ["C","F","H","J","K"]
 
+var arr4 = ["A","D","G","J","M","P","S","V","Y","AB"]
+var arr5 = ["B","E","H","K","N","Q","T","W"]
+var arr6 = ["C","F","I","L","O","R","U","X","AA","AD","AE","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A"]
+
+
 var full = [arr1,arr2,arr3]
 var arr = full.flatMap {
     return $0
 }
+print(arr)
 
 let count = full.count
 
-var ans : [String] = Array()
 
 
 
-func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String]) -> [String]{
+func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String],size : Int) -> [String]{
     var greatest = 0
     var full = [arr1,arr2,arr3]
-    var arr = full.flatMap {
-        return $0
-    }
-    
+    var ans : [String] = Array()
+
     full.map { arr in
         if arr.count > greatest {
             greatest = arr.count
@@ -32,14 +35,24 @@ func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String]) -> [Strin
         for y in 0..<full.count{
             print(iteration)
             iteration += 1
-            
             if x < full[y].count {
                 ans.append(full[y][x])
+                if iteration == size{
+                 return ans
+                }
             }
-            
         }
     }
     return ans
 }
 
-distributeBrute(arr1: arr2, arr2: arr2, arr3: arr3)
+//distributeBrute(arr1: arr1, arr2: arr2, arr3: arr3)
+let distributed = distributeBrute(arr1: arr4, arr2: arr5, arr3: arr6,size: 10)
+print(distributed)
+
+let edge1 = ["a"]
+let edge2 = ["b"]
+let edge3 = ["c"]
+let edgeAnswer1 = distributeBrute(arr1: edge1, arr2: edge2, arr3: edge3, size: 10)
+print(edgeAnswer1)
+
