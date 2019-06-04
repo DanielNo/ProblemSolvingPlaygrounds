5a68cbf121efe789680336f6bfe74d235b7c4390
Daniel No <dno@webmd.net>
2019-06-04 13:42:09 -0400

    test for edge cases

--- Distribution.playground/Contents.swift
+++ Distribution.playground/Contents.swift
@@ -1,26 +1,8 @@
 import UIKit
-
-var arr1 = ["A","D","G","I"]
-var arr2 = ["B","E"]
-var arr3 = ["C","F","H","J","K"]
-
-var arr4 = ["A","D","G","J","M","P","S","V","Y","AB"]
-var arr5 = ["B","E","H","K","N","Q","T","W"]
-var arr6 = ["C","F","I","L","O","R","U","X","AA","AD","AE","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A"]
-
-
-var full = [arr1,arr2,arr3]
-var arr = full.flatMap {
-    return $0
-}
-print(arr)
-
-let count = full.count
-
-
-
-
-func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String],size : Int) -> [String]{
+func distributeContent(arr1 : [String],arr2 : [String], arr3 : [String],size : Int) -> [String]{
+    if size <= 0{
+        return [String]()
+    }
     var greatest = 0
     var full = [arr1,arr2,arr3]
     var ans : [String] = Array()
@@ -33,11 +15,11 @@ func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String],size : Int
     var iteration = 0
     for x in 0..<greatest{
         for y in 0..<full.count{
-            print(iteration)
             iteration += 1
             if x < full[y].count {
                 ans.append(full[y][x])
                 if iteration == size{
+                    print("iterations : \(iteration)")
                  return ans
                 }
             }
@@ -46,13 +28,35 @@ func distributeBrute(arr1 : [String],arr2 : [String], arr3 : [String],size : Int
     return ans
 }
 
-//distributeBrute(arr1: arr1, arr2: arr2, arr3: arr3)
-let distributed = distributeBrute(arr1: arr4, arr2: arr5, arr3: arr6,size: 10)
-print(distributed)
+
+var case1 = ["A","D","G","I"]
+var case2 = ["B","E"]
+var case3 = ["C","F","H","J","K"]
+let distributed1 = distributeContent(arr1: case1, arr2: case2, arr3: case3, size: 10)
+
+
+var case4 = ["A","D","G","J","M","P","S","V","Y","AB"]
+var case5 = ["B","E","H","K","N","Q","T","W"]
+var case6 = ["C","F","I","L","O","R","U","X","AA","AD","AE","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A"]
+let distributed2 = distributeContent(arr1: case4, arr2: case5, arr3: case6,size: 10)
+
+
 
 let edge1 = ["a"]
 let edge2 = ["b"]
 let edge3 = ["c"]
-let edgeAnswer1 = distributeBrute(arr1: edge1, arr2: edge2, arr3: edge3, size: 10)
-print(edgeAnswer1)
+let edgeCase1 = distributeContent(arr1: edge1, arr2: edge2, arr3: edge3, size: -1)
+
+
+let edge4 : [String] = Array()
+let edgeCase2 = distributeContent(arr1: edge4, arr2: edge4, arr3: edge4, size: 10)
+
+
+let edge5 = ["a"]
+let edge6 = ["b","d"]
+let edge7 = ["c","e","f"]
+let edgeCase3 = distributeContent(arr1: edge5, arr2: edge6, arr3: edge7, size: 10)
+let edgeCase4 = distributeContent(arr1: edge7, arr2: edge6, arr3: edge5, size: 10)
+
+
 
