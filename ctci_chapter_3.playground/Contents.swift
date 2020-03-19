e68ef312becaa3273ca7f4859e088fec86dba72a
Daniel No <mr.danielno@gmail.com>
2020-03-18 20:25:15 -0400

    add description

--- ctci_chapter_3.playground/Contents.swift
+++ ctci_chapter_3.playground/Contents.swift
@@ -4,6 +4,28 @@ import UIKit
 
 import UIKit
 
+// A linked list
+public class ListNode {
+  
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+  
+  public func print() -> String{
+      var str = String(self.val)
+      var node = self
+      while node.next != nil {
+          node = node.next!
+          str+="->\(String(node.val))"
+      }
+      return str
+  }
+}
+
+// Node for stack or queue
 public class Node : CustomStringConvertible{
     public var description: String
     
