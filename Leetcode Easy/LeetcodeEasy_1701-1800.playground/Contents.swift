7268b78d359b5a26aa07b62a05bc4f226814bd24
Daniel No <mr.danielno@gmail.com>
2025-07-09 17:26:42 -0400

    been a while leetcode 75 easy

--- Leetcode Easy/LeetcodeEasy_1701-1800.playground/Contents.swift
+++ Leetcode Easy/LeetcodeEasy_1701-1800.playground/Contents.swift
@@ -1,2 +1,31 @@
 import UIKit
+/*
 
+
+
+*/
+    func mergeAlternately(_ word1: String, _ word2: String) -> String {
+        let chars1 = word1.map { String($0) }
+        let chars2 = word2.map { String($0) }
+
+
+        let limit1 = chars1.count
+        let limit2 = chars2.count
+
+        var answer = Array(repeating:"", count: limit1 + limit2)
+        let max = limit1 + limit2
+        var i = 0
+        var j = 0
+        while (i + j < max) {
+            if i < limit1 {
+                answer[i+j] = chars1[i]
+                i += 1
+            }
+            if j < limit2 {
+                answer[i+j] = chars2[j]
+                j += 1
+            }
+        }
+        let answerString = answer.joined()
+        return answerString
+    }
