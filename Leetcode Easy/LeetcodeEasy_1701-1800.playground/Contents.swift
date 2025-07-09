05983471ef3c3c0aad68873788220a4a33aa0362
Daniel No <mr.danielno@gmail.com>
2025-07-09 17:32:42 -0400

    Update Contents.swift

--- Leetcode Easy/LeetcodeEasy_1701-1800.playground/Contents.swift
+++ Leetcode Easy/LeetcodeEasy_1701-1800.playground/Contents.swift
@@ -1,6 +1,18 @@
 import UIKit
 /*
+1768. Merge Strings Alternately
 
+Approach: Create a empty string ("") character/string array and overwrite the values so we have the complete merged string that is alternating. 
+Iterate through the values in a while loop.
+Initially I used a single counter, which was wrong and skipped some iterations. Took a little hint from solutions to use two pointers, and I was able to get it easily.
+
+Mini cheating:
+Looked up:
+- String to String array = map function
+- String array to string = joined()
+
+Time Complexity: O(n + m) where n and m are lengths of each word
+Space Complexity: O(n + m) which is the space of the String array
 
 
 */
